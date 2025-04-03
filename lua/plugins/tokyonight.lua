7790aed1a3e14f981c573d061d0775c5eaf7f3b3
mrprohack <mrprohack@gmail.com>
2025-04-03 10:14:42 +0530

    fix nvim errurs

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,11 +1,9 @@
--- Transparent background for LazyVim
-vim.opt.background = "dark"
-vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
-vim.cmd([[hi NormalNC guibg=NONE ctermbg=NONE]])
-vim.cmd([[hi EndOfBuffer guibg=NONE ctermbg=NONE]])
-
--- Example: Transparency for tokyonight colorscheme
-require("tokyonight").setup({
-  transparent = true,
-})
-vim.cmd([[colorscheme tokyonight]])
+return {
+  "folke/tokyonight.nvim",
+  config = function()
+    require("tokyonight").setup({
+      transparent = true,
+    })
+    vim.cmd([[colorscheme tokyonight]])
+  end,
+}
